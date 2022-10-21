d2e9f1eb7720b1b0c4f369abb731124cd455e564
Caroline <C.klakegg@gmail.com>
2022-10-21 11:41:52 +0200

    Initial Commit

---
+++ Cosmics_ShortCut/Cosmics_ShortCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cosmics_ShortCutApp.swift
+//  Cosmics_ShortCut
+//
+//  Created by Caroline Klakegg on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cosmics_ShortCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
